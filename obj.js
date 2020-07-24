a5e58e18cd02b93a22f1196d2c7c11f82c1a60fb
Atikin10 <ivan.pavlik.10@bk.ru>
2020-07-24 20:39:35 +0300

    Update obj.js
    переменные a=10 и b=2. Выведите в консоль их сумму, разность, произведение и частное (результат деления).

--- obj.js
+++ obj.js
@@ -1,4 +1,13 @@
-let num = 3;
-console.log(num);
+let a=10;
+let b=2;
+let s=a+b;
+let r=a-b;;
+let p=a*b;
+let d=a/b;
+console.log("Сумма="+s);
+console.log("Разность="+r);
+console.log("Произведение="+p);
+console.log("Частное="+d);
+
 
 
