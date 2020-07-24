dcab54927d45dabf5fee1b206d08c44317a1f47a
Atikin10 <ivan.pavlik.10@bk.ru>
2020-07-24 20:42:09 +0300

    Update obj.js
    переменные c=15 и d=2. Просуммируйте их, а результат присвойте переменной result. Выведите в консоль значение переменной result.

--- obj.js
+++ obj.js
@@ -1,13 +1,7 @@
-let a=10;
-let b=2;
-let s=a+b;
-let r=a-b;;
-let p=a*b;
-let d=a/b;
-console.log("Сумма="+s);
-console.log("Разность="+r);
-console.log("Произведение="+p);
-console.log("Частное="+d);
+let c=15;
+let d=2;
+let result=c+d;
+console.log("Сумма="+result)
 
 
 
